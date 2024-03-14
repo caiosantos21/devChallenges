0cb4211e7084a43dedf2df196babad8b415e18bf
Caio Santos <caiosantos21@gmail.com>
2024-03-14 16:30:40 -0300

    Time: 55 ms (71.43%) | Memory: 51.4 MB (62.86%) - LeetSync

---
+++ challenges/leetcode/2571-find-the-pivot-integer/find-the-pivot-integer.ts
@@ -0,0 +1,15 @@
+function pivotInteger(n: number): number {
+    let num = -1
+
+    for (let i = 1; i <= n; i++) {
+
+        let s1 = i * (i + 1) / 2
+        let s2 = (n-(i-1)) * (i + n) / 2
+
+        if (s1 === s2) {
+            num = i
+        }
+    }
+
+    return num
+};
