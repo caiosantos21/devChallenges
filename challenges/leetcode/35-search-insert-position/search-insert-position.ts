a226fd0d79c5d78d3bf3f4e846f8e2c8853ffa71
Caio Santos <caiosantos21@gmail.com>
2024-03-16 14:03:34 -0300

    Time: 57 ms (56.88%) | Memory: 51.4 MB (56.83%) - LeetSync

--- challenges/leetcode/35-search-insert-position/search-insert-position.ts
+++ challenges/leetcode/35-search-insert-position/search-insert-position.ts
@@ -1,18 +1,35 @@
 function searchInsert(nums: number[], target: number): number {
-    if (nums[nums.length - 1] < target) {
-        return nums.length
-    }
+    // if (nums[nums.length - 1] < target) {
+    //     return nums.length
+    // }
 
-    if (nums[0] >= target) {
-        return 0
-    }
+    // if (nums[0] >= target) {
+    //     return 0
+    // }
 
+    // for (let i = 1; i <= nums.length - 1; i++) {
+    //     if (nums[i] >= target) {
+    //         return i
+    //     }
+    // }
 
-    for (let i = 1; i <= nums.length - 1; i++) {
-        if (nums[i] >= target) {
-            return i
+    let low = 0;
+    let high = nums.length - 1;
+
+    while (high >= low) {
+        let mid = ~~(low + (high - low) / 2);
+
+        if (target == nums[mid]) {
+            return mid;
+        }
+        else if (target > nums[mid]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
         }
     }
 
 
+    return low;
 };
