08f0a06c5e569be34fce5d3698a91a61473c815e
Caio Santos <caiosantos21@gmail.com>
2024-03-16 15:13:45 -0300

    Time: 110 ms (68.59%) | Memory: 56.9 MB (90.64%) - LeetSync

--- challenges/leetcode/2-add-two-numbers/add-two-numbers.ts
+++ challenges/leetcode/2-add-two-numbers/add-two-numbers.ts
@@ -11,32 +11,20 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-
-
-
     const handleSum = (a1: ListNode, a2: ListNode, extra: number) => {
         const node = new ListNode()
 
         const sum = a1.val + a2.val + extra
-        const newExtra = sum >= 10 ? 1 : 0
+        const newExtra = ~~(sum / 10)
 
-        node.val = sum >= 10 ? sum - 10 : sum
+        node.val = sum % 10
+        const aux = new ListNode()
 
-        if (a1.next && a2.next) {
-            node.next = handleSum(a1.next, a2.next, newExtra)
+        if (a1.next || a2.next) {
+            node.next = handleSum(a1.next ? a1.next : aux, a2.next ? a2.next : aux, newExtra)
         }
 
-        if (a1.next && !a2.next) {
-            const aux = new ListNode()
-            node.next = handleSum(a1.next, aux, newExtra)
-        }
-
-        if (!a1.next && a2.next) {
-            const aux = new ListNode()
-            node.next = handleSum(aux, a2.next, newExtra)
-        }
-        if (!a1.next && !a2.next && newExtra>0) {
-            const aux = new ListNode()
+        if (!a1.next && !a2.next && newExtra > 0) {
             node.next = handleSum(aux, aux, newExtra)
         }
 
