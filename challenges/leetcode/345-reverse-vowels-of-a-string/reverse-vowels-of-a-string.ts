67a46d931481f9c1749f34b2e8454d8bb370309c
Caio Santos <caiosantos21@gmail.com>
2024-03-20 11:05:45 -0300

    Time: 60 ms (95.13%) | Memory: 55.1 MB (96.20%) - LeetSync

--- challenges/leetcode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.ts
+++ challenges/leetcode/345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.ts
@@ -1,23 +1,37 @@
 function reverseVowels(s: string): string {
-    let indexes = [];
+    let l = 0
+    let r = s.length - 1
+    let a = s.split('')
+    let aL = ''
+    let aR = ''
 
-    for (let i = 0; i < s.length; i++) {
-        if (['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', "O", "U"].indexOf(s[i]) > -1) {
-            indexes.push(s[i]);
-        }
-    }
 
-    let j = indexes.length - 1
-    let a = ''
-
-    for (let i = 0; i < s.length; i++) {
-        if (indexes.indexOf(s[i]) > -1) {
-            a = a + indexes[j]
-            j--
+    while (l <= r) {
+        if (['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', "O", "U"].includes(a[l])) {
+            aL = a[l]
         } else {
-            a = a + s[i]
+            l++
+        }
+        if (['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', "O", "U"].includes(a[r])) {
+            aR = a[r]
+        } else {
+            r--
+        }
+        if (aL != '' && aR != '') {
+
+            a[l] = aR
+            a[r] = aL
+
+            aL = ''
+            aR = ''
+            r--
+            l++
+
         }
     }
 
-    return a
+
+
+
+    return a.join('')
 };
