03f4d81554241f874f66bae0cc3906d0bbb4dde6
Caio Santos <caiosantos21@gmail.com>
2024-03-20 00:51:28 -0300

    Time: 57 ms (84.48%) | Memory: 52 MB (95.24%) - LeetSync

--- challenges/leetcode/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
+++ challenges/leetcode/1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.ts
@@ -1,9 +1,5 @@
 function kidsWithCandies(candies: number[], extraCandies: number): boolean[] {
-    let max = candies[0]
-
-    for (let i = 1; i < candies.length; i++) {
-        max = Math.max(max, candies[i])
-    }
+    let max = Math.max(...candies)
 
     let aux = []
     for (let i = 0; i < candies.length; i++) {
