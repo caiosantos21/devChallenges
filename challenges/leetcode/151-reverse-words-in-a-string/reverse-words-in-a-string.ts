23b7210651088c6586510aacf725fdc67cca2d3c
Caio Santos <caiosantos21@gmail.com>
2024-03-20 13:32:16 -0300

    Time: 60 ms (55.30%) | Memory: 52.1 MB (28.56%) - LeetSync

--- challenges/leetcode/151-reverse-words-in-a-string/reverse-words-in-a-string.ts
+++ challenges/leetcode/151-reverse-words-in-a-string/reverse-words-in-a-string.ts
@@ -1,12 +1,19 @@
 function reverseWords(s: string): string {
-    let aux = s.split(' ')
-    let r = []
+    let a = ''
+    let b = ''
 
-    for (let i = aux.length - 1; i >= 0; i--) {
-        if (aux[i] != '') {
-            r.push(aux[i])
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== ' ') {
+            a = s[i] + a
+
+        }
+
+        if (s[i] === ' ' || i === 0) {
+            b = b + ((a.length > 0 && b.length > 0) ? ' ' : '') + a
+            a = ''
         }
     }
 
-    return r.join(' ')
+
+    return b
 };
