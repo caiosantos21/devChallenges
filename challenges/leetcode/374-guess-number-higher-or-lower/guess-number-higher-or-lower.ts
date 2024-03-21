af04c4009881ec37f7129634e2772200f94094f2
Caio Santos <caiosantos21@gmail.com>
2024-03-21 00:34:58 -0300

    Time: 58 ms (36.82%) | Memory: 50.1 MB (49.58%) - LeetSync

---
+++ challenges/leetcode/374-guess-number-higher-or-lower/guess-number-higher-or-lower.ts
@@ -0,0 +1,27 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+
+function guessNumber(n: number): number {
+    let min = 1
+    let max = n
+
+    while (min <= max) {
+        let mid = min + (~~((max - min) / 2))
+        
+        if (guess(mid) === 0) {
+            return mid
+        } else if (guess(mid) === -1) {
+            max = mid - 1
+        } else if (guess(mid) === 1) {
+            min = mid + 1
+        }
+
+    }
+};
