4411a924924dab668088b2e0c1479f8dd6f757f0
Caio Santos <caiosantos21@gmail.com>
2024-03-21 14:00:21 -0300

    Time: 139 ms (9.83%) | Memory: 55.5 MB (94.02%) - LeetSync

--- challenges/leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
+++ challenges/leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
@@ -1,16 +1,15 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
     let m1 = new Map().set(0, []).set(1, [])
 
-    const check = (i: number, arr1: number[], arr2: number[], key: number) => {
-        if (i < arr1.length && arr2.indexOf(arr1[i]) === -1 && m1.get(key).indexOf(arr1[i]) === -1) {
-            // m1.set(key, [...m1.get(key), arr1[i]])
-            m1.get(key).push(arr1[i])
+    const check = (v: number, arr2: number[], key: number) => {
+        if (arr2.indexOf(v) === -1 && m1.get(key).indexOf(v) === -1) {
+            m1.get(key).push(v)
         }
     }
 
     for (let i = 0; i < Math.max(nums1.length, nums2.length); i++) {
-        check(i, nums1, nums2, 0)
-        check(i, nums2, nums1, 1)
+        i < nums1.length && check(nums1[i], nums2, 0)
+        i < nums2.length && check(nums2[i], nums1, 1)
     }
 
     return [...m1.values()]
