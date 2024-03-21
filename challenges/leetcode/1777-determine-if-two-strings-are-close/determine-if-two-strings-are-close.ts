d817b2eab4e6590e81e7ada6175de523df7ccf23
Caio Santos <caiosantos21@gmail.com>
2024-03-21 17:01:13 -0300

    Time: 137 ms (61.87%) | Memory: 58.2 MB (81.95%) - LeetSync

--- challenges/leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
+++ challenges/leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.ts
@@ -6,12 +6,10 @@ function closeStrings(word1: string, word2: string): boolean {
     const m1 = new Map()
     const m2 = new Map()
 
-    for (let i = 0; i < word1.length; i++) {
-        m1.set(word1[i], (m1.get(word1[i]) || 0) + 1)
-    }
+    for (let i = 0; i < Math.max(word1.length,word2.length); i++) {
+        i < word1.length && m1.set(word1[i], (m1.get(word1[i]) || 0) + 1)
 
-    for (let j = 0; j < word2.length; j++) {
-        m2.set(word2[j], (m2.get(word2[j]) || 0) + 1)
+        i < word2.length && m2.set(word2[i], (m2.get(word2[i]) || 0) + 1)
     }
 
     if ([...m1.keys()].sort().join('') != [...m2.keys()].sort().join('')) {
