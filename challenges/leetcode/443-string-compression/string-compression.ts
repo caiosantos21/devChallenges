df56ee26b633a41c4042aed37edcc14d8e33a8e1
Caio Santos <caiosantos21@gmail.com>
2024-03-21 02:05:02 -0300

    Time: 53 ms (98.26%) | Memory: 51.7 MB (100.00%) - LeetSync

---
+++ challenges/leetcode/443-string-compression/string-compression.ts
@@ -0,0 +1,25 @@
+function compress(chars: string[]): number {
+    const mp = new Map()
+    const arr = []
+    let aux = 1
+
+
+    for (let i = 0; i <= chars.length; i++) {
+        if (chars[i] === chars[i + 1]) {
+            aux++
+        } else {
+            arr.push(chars[i])
+            if (aux > 1) {
+                arr.push(...aux.toString().split(''))
+            }
+            aux = 1
+        }
+    }
+    chars.length = 0
+
+    arr.forEach((val) => {
+        chars.push(val)
+    })
+
+    return chars.length
+};
