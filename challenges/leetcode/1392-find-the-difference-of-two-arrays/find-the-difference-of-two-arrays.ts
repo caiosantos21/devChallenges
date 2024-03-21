1e58286fa13c73fef5347915f2139066a12319f8
Caio Santos <caiosantos21@gmail.com>
2024-03-21 13:51:59 -0300

    Time: 161 ms (5.41%) | Memory: 57 MB (59.97%) - LeetSync

--- challenges/leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
+++ challenges/leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.ts
@@ -1,16 +1,16 @@
 function findDifference(nums1: number[], nums2: number[]): number[][] {
-    let m1 = new Map()
-    let m2 = new Map()
+    let m1 = new Map().set(0, []).set(1, [])
 
-    for (let i = 0; i < Math.max(nums1.length, nums2.length); i++) {
-        if (i < nums1.length && nums2.indexOf(nums1[i]) === -1) {
-            m1.set(nums1[i], 1)
-        }
-
-        if (i < nums2.length && nums1.indexOf(nums2[i]) === -1) {
-            m2.set(nums2[i], 1)
+    const check = (i: number, arr1: number[], arr2: number[], key: number) => {
+        if (i < arr1.length && arr2.indexOf(arr1[i]) === -1 && m1.get(key).indexOf(arr1[i]) === -1) {
+            m1.set(key, [...m1.get(key), arr1[i]])
         }
     }
 
-    return [[...m1.keys()], [...m2.keys()]]
+    for (let i = 0; i < Math.max(nums1.length, nums2.length); i++) {
+        check(i, nums1, nums2, 0)
+        check(i, nums2, nums1, 1)
+    }
+
+    return [...m1.values()]
 };
