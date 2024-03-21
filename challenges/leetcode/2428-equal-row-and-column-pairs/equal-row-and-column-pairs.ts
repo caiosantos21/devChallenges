3e3eac66107b27602823e9f8c55279cfe7397739
Caio Santos <caiosantos21@gmail.com>
2024-03-21 18:09:05 -0300

    Time: 116 ms (46.74%) | Memory: 65.7 MB (11.96%) - LeetSync

--- challenges/leetcode/2428-equal-row-and-column-pairs/equal-row-and-column-pairs.ts
+++ challenges/leetcode/2428-equal-row-and-column-pairs/equal-row-and-column-pairs.ts
@@ -1,26 +1,17 @@
 function equalPairs(grid: number[][]): number {
     let m1 = new Map()
     let m2 = new Map()
-    let gridLength = grid.length
-    let rowLength = grid[0].length
 
-    for (let i = 0; i < gridLength; i++) {
+    for (let i = 0; i < grid.length; i++) {
         let row = ''
+        let col = ''
 
-        for (let j = 0; j < rowLength; j++) {
+        for (let j = 0; j < grid.length; j++) {
             row += (j > 0 ? '.' : '') + grid[i][j]
+            col += (j > 0 ? '.' : '') + grid[j][i]
         }
 
         m1.set(row, (m1.get(row) || 0) + 1)
-    }
-
-    for (let i = 0; i < rowLength; i++) {
-        let col = ''
-
-        for (let j = 0; j < gridLength; j++) {
-            col += (j > 0 ? '.' : '') + grid[j][i]
-        }
-        
         m2.set(col, (m2.get(col) || 0) + 1)
     }
 
