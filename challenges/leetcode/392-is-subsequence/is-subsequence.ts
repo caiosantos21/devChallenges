d895a7d6cbf3f4aa5d11637a707a398a9ff6a3e1
Caio Santos <caiosantos21@gmail.com>
2024-03-22 10:38:45 -0300

    Time: 64 ms (26.75%) | Memory: 50.5 MB (73.78%) - LeetSync

---
+++ challenges/leetcode/392-is-subsequence/is-subsequence.ts
@@ -0,0 +1,26 @@
+function isSubsequence(s: string, t: string): boolean {
+    if (s === t) {
+        return true
+    }
+    
+    if (s.length > t.length) {
+        return false
+    }
+
+    let i = 0
+    let j = 0
+
+    while (j < t.length) {
+        if (s[i] === t[j]) {
+            i++
+        }
+
+        j++
+
+        if (i === s.length) {
+            return true
+        }
+    }
+
+    return false
+};
